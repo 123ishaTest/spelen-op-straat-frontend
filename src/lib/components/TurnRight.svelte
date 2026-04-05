73319a0643d88846855e72486b9b5b087031c556
Isha Dijcks <ishadijcks@gmail.com>
2026-04-05 13:48:14 +0200

    Add layout

---
+++ src/lib/components/TurnRight.svelte
@@ -0,0 +1,13 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="#ffffff"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class="lucide lucide-corner-up-right-icon lucide-corner-up-right h-16 w-16"
+>
+  <path d="m15 14 5-5-5-5" />
+  <path d="M4 20v-7a4 4 0 0 1 4-4h12" />
+</svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,94 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import TurnLeft from '$lib/components/TurnLeft.svelte';
+  import TurnRight from '$lib/components/TurnRight.svelte';
+  import { onMount } from 'svelte';
+  import { Direction } from '$lib/model/Direction.ts';
+
+  let isLeftDown = $state(false);
+  let isRightDown = $state(false);
+  let latest: Direction = $state(Direction.NONE);
+
+  let direction: Direction = $derived.by(() => {
+    if (isLeftDown && isRightDown) {
+      return latest;
+    }
+    if (isLeftDown) {
+      return Direction.LEFT;
+    }
+    if (isRightDown) {
+      return Direction.RIGHT;
+    }
+
+    return Direction.NONE;
+  });
+
+  const downLeft = () => {
+    isLeftDown = true;
+    latest = Direction.LEFT;
+  };
+  const upLeft = () => {
+    isLeftDown = false;
+  };
+
+  const downRight = () => {
+    isRightDown = true;
+    latest = Direction.RIGHT;
+  };
+  const upRight = () => {
+    isRightDown = false;
+  };
+
+  const sendDirection = (direction: Direction) => {
+    // TODO(@Isha): Websocket send
+    webSocket.send(
+      JSON.stringify({
+        type: 'input',
+        direction: direction,
+      }),
+    );
+  };
+
+  const webSocket = new WebSocket('ws://localhost:9080');
+
+  onMount(() => {
+    return setInterval(() => {
+      sendDirection(direction);
+    }, 100);
+  });
+</script>
+
+<div class="grass flex h-screen touch-none flex-col select-none">
+  <div class="flex flex-row justify-center border-b-4 border-white">
+    <span class="p-4 text-xl font-bold text-white">Spelen op Straat</span>
+  </div>
+  <div class="flex h-full flex-row">
+    <div
+      role="button"
+      tabindex="-2"
+      class="flex flex-1 cursor-pointer flex-col items-center justify-center {isLeftDown ? 'bg-gray-600/20' : ''}"
+      onpointerdown={downLeft}
+      onpointerup={upLeft}
+    >
+      <TurnLeft />
+    </div>
+
+    <div class="border-2 border-white"></div>
+    <div
+      role="button"
+      tabindex="-1"
+      class="flex flex-1 cursor-pointer flex-col items-center justify-center {isRightDown ? 'bg-gray-600/20' : ''}"
+      onpointerdown={downRight}
+      onpointerup={upRight}
+    >
+      <TurnRight />
+    </div>
+  </div>
+</div>
+
+<style>
+  .grass {
+    background: repeating-linear-gradient(90deg, #31d978, #31d978 20px, #2ecc71 20px, #2ecc71 40px);
+    background-size: 40px 100%;
+    background-position: center;
+  }
+</style>
