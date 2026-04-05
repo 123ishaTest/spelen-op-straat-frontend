e3e9cf07375042e7e3fd221d210e1026770753c4
Isha Dijcks <ishadijcks@gmail.com>
2026-04-05 17:41:17 +0200

    Add Websocket connection

---
+++ src/lib/components/ConnectionStatus.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import WifiConnected from '$lib/components/icons/WifiConnected.svelte';
+  import WifiDisconnected from '$lib/components/icons/WifiDisconnected.svelte';
+
+  interface Props {
+    isConnected: boolean;
+  }
+
+  let { isConnected }: Props = $props();
+</script>
+
+{#if isConnected}
+  <WifiConnected />
+{:else}
+  <WifiDisconnected />
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-  import TurnLeft from '$lib/components/TurnLeft.svelte';
-  import TurnRight from '$lib/components/TurnRight.svelte';
+  import TurnLeft from '$lib/components/icons/TurnLeft.svelte';
+  import TurnRight from '$lib/components/icons/TurnRight.svelte';
   import { onMount } from 'svelte';
   import { Direction } from '$lib/model/Direction.ts';
+  import ConnectionStatus from '$lib/components/ConnectionStatus.svelte';
+  import { PUBLIC_WEBSOCKET_SERVER } from '$env/static/public';
 
   let isLeftDown = $state(false);
   let isRightDown = $state(false);
@@ -48,18 +50,33 @@
     );
   };
 
-  const webSocket = new WebSocket('ws://localhost:9080');
+  const webSocket = new WebSocket(PUBLIC_WEBSOCKET_SERVER);
+  let isConnected = $state(false);
+
+  webSocket.addEventListener('open', () => {
+    isConnected = true;
+  });
+  webSocket.addEventListener('close', () => {
+    isConnected = false;
+  });
+  webSocket.addEventListener('error', (e) => {
+    console.warn(e);
+  });
 
   onMount(() => {
     return setInterval(() => {
+      if (!isConnected) {
+        return;
+      }
       sendDirection(direction);
     }, 100);
   });
 </script>
 
 <div class="grass flex h-screen touch-none flex-col select-none">
-  <div class="flex flex-row justify-center border-b-4 border-white">
+  <div class="flex flex-row items-center justify-center border-b-4 border-white">
     <span class="p-4 text-xl font-bold text-white">Spelen op Straat</span>
+    <ConnectionStatus {isConnected} />
   </div>
   <div class="flex h-full flex-row">
     <div
